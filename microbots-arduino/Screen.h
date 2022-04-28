722e1d7b96b9ad3bc779817f64e1653663767a12
sanine <sanine.not@pm.me>
2022-04-28 17:12:20 -0500

    begin custom serial library

--- microbots-arduino/Screen.h
+++ microbots-arduino/Screen.h
@@ -5,6 +5,8 @@
 #include <Diablo_Const4D.h>
 #include <Diablo_Serial_4DLib.h>
 
+#define SERIAL_TIMEOUT 2000
+
 namespace smm {
 	class Screen {
 		public:
@@ -12,7 +14,6 @@ namespace smm {
 		       unsigned int width=800,
 		       unsigned int height=480)
 			: mSerial(rxPin, txPin),
-			  mDisplay(((Stream*) &mSerial)),
 			  mResetPin(resetPin),
 			  mWidth(width), mHeight(height), mPosition(0),
 			  mPrevious(-1)
@@ -20,93 +21,139 @@ namespace smm {
 
 		void setup()
 		{
+			/* initialize software serial */
 			mSerial.begin(9600);
 
+			/* ensure screen is reset */
 			pinMode(mResetPin, OUTPUT);
 			digitalWrite(mResetPin, 0);
 			delay(100);
 			digitalWrite(mResetPin, 1);
 			delay(3000);
 
-			mDisplay.gfx_ScreenMode(LANDSCAPE);
-			mDisplay.gfx_Cls();
+			enum serial_result_t r;
 
-			mDisplay.putstr("display ready!");
+			/* set screen to landscape mode */
+			byte landscape[] = {0, 0};
+			r = command_response(0xff, 0x42, landscape, 2, NULL);
+			if (r != SR_OK)
+				serial_printf("screen mode failed: %d\n", r);
+			else
+				serial_printf("set screen to landscape\n");
 
-			Serial.println("=== begin fs operations ===");
+			/* clear screen */
+			r = command(0xff, 0x82, NULL, 0);
+			if (r != SR_OK)
+				serial_printf("clear screen failed: %d\n", r);
+			else
+				serial_printf("cleared screen\n");
 
-			const int max_retries = 15;
-			int tries = 0;
-
-			Serial.println(mSerial.available());
-			while(mSerial.available()) {
-				mSerial.read();
-			}
-			byte buf[] = { 0x00, 0x18, 0x48, 0x6c, 0x6c, 0x6f, 0x00 };
-			mSerial.write(buf, sizeof(buf));
-			while(mSerial.available() < 3);
-			Serial.print("available: ");
-			Serial.println(mSerial.available());
-			while(mSerial.available()) {
-				Serial.print("0x");
-				Serial.println(mSerial.read(), HEX);
-			}
-
-
-			int err = mDisplay.file_Mount();
-			while(tries < max_retries && err == 0) {
-				Serial.println("mount failed, retrying...");
-				err = mDisplay.file_Mount();
-				tries += 1;
-			}
-			if (err == 0) {
-				Serial.println("maximum attempts exceded. mount failed.");
-				goto finish;
-			}
+			/* mount FAT16 fs */
+			word success;
+			r = command_response(0xfe, 0x3c, NULL, 0, &success);
+			if (r != SR_OK)
+				serial_printf("mount fs failed: %d\n", r);
+			else if (success == 0)
+				serial_printf("mount fs failed!\n");
+			else
+				serial_printf("mounted fs\n");
 
 
-			word handle = mDisplay.file_Open("img.gci", 'r');
-			Serial.print("image handle: ");
-			Serial.println(handle);
-			err = mDisplay.file_Error();
-			if (err != FE_OK) {
-				Serial.print("failed to get file handle: ");
-				Serial.println(err);
-				goto finish;
-			}
-			mDisplay.file_Image(0, 0, handle);
-			err = mDisplay.file_Error();
-			if (err != FE_OK) {
-				Serial.print("failed to get display image: ");
-				Serial.println(err);
-			}
-finish:
-			Serial.println("=== finish fs operations ===");
+			/* open image file */
+			const char * fname = "microb~1.gci\0r";
+			word handle;
+			r = command_response(0x00, 0x0a, (byte*) fname, 14, &handle);
+			if (r != SR_OK)
+				serial_printf("open '%s' failed: %d\n", fname, r);
+			else if (handle == 0)
+				serial_printf("could not find file '%s'\n", fname);
+			else
+				serial_printf("opened '%s'\n", fname);
+
+
+			/* display image */
+			word error;
+			byte xy_handle[] = {
+				0, 0, /* x pos */
+				0, 0, /* y pos */
+				handle >> 8, handle,
+			};
+			r = command_response(0xfe, 0x4a, xy_handle, 6, &error);
+			if (r != SR_OK)
+				serial_printf("display image failed: %d\n", r);
+			else if (error != 0)
+				serial_printf("display image failed due to file error: %d\n");
+			else
+				serial_printf("displayed image\n");
 		}
 
-		void push(double v)
-		{
-			int value = (int) (v * mHeight);
-
-			if (mPrevious < 0)
-				mPrevious = value;
-			else {
-				mDisplay.gfx_Line(mPosition, mPrevious,
-				                  ++mPosition, value,
-				                  GREEN);
-				mPrevious = value;
-				if (mPosition >= mWidth) {
-					mPosition = 0;
-					mDisplay.gfx_Cls();
-				}
-			}
-		}
 
 		private:
 		SoftwareSerial mSerial;
-		Diablo_Serial_4DLib mDisplay;
 		int mResetPin;
-		unsigned int mWidth, mHeight, mPosition;
-		int mPrevious;
+
+		enum serial_result_t {
+			SR_OK,
+			SR_NOACK,
+			SR_TIMEOUT,
+		};
+
+		/* destroy existing values in the serial buffer */
+		void empty_serial_buffer() {
+			while(mSerial.available()) Serial.read();
+		}
+
+		enum serial_result_t getack() {
+			long start_time = millis();
+			while (millis() - start_time < SERIAL_TIMEOUT) {
+				if (mSerial.available()) {
+					byte b = mSerial.read();
+					if (b == 0x06) return SR_OK;
+					else return SR_NOACK;
+				}
+			}
+			return SR_TIMEOUT;
+		}
+
+		enum serial_result_t getword(word *w) {
+			long start_time = millis();
+			while (millis() - start_time < SERIAL_TIMEOUT) {
+				if (mSerial.available() == 2) {
+					byte hi = mSerial.read();
+					byte lo = mSerial.read();
+					if (w != NULL)
+						*w = (hi << 8) | lo;
+					return SR_OK;
+				}
+			}
+			return SR_TIMEOUT;
+		}
+
+		enum serial_result_t command(byte hi, byte lo, byte *data, size_t num) {
+			empty_serial_buffer();
+			mSerial.write(hi);
+			mSerial.write(lo);
+			for (int i=0; i<num; i++)
+				mSerial.write(data[i]);
+			mSerial.flush();
+			return getack();
+		}
+
+		enum serial_result_t command_response(byte hi, byte lo, byte *data, size_t num, word *response) {
+			enum serial_result_t r = command(hi, lo, data, num);
+			if (r != SR_OK) return r;
+			r = getword(response);
+			return r;
+		}
+
+		void serial_printf(const char *format, ...) {
+			va_list args;
+			va_start(args, format);
+			char buffer[128];
+			vsprintf(buffer, format, args);
+			va_end(args);
+			Serial.print(buffer);
+			Serial.flush();
+		}
 	};
 }
