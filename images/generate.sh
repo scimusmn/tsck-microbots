7d71a8d9e1fbdf189b30463a2132948fb572a57c
sanine <sanine.not@pm.me>
2022-05-23 14:54:16 -0500

    remove spurious files and center digits

--- images/generate.sh
+++ images/generate.sh
@@ -2,9 +2,9 @@
 
 gci \
 	--label "background" -i gradient-bg.png \
-	--label "digit0" --background "gradient-bg.png" --offset 212x297 --array 10x1 -i digits.png \
-	--label "digit1" --background "gradient-bg.png" --offset 293x297 --array 10x1 -i digits.png \
-	--label "colon"  --background "gradient-bg.png" --offset 374x297 -i colon.png \
-	--label "digit2" --background "gradient-bg.png" --offset 455x297 --array 10x1 -i digits.png \
-	--label "digit3" --background "gradient-bg.png" --offset 536x297 --array 10x1 -i digits.png \
+	--label "digit0" --background "gradient-bg.png" --offset 192x297 --array 10x1 -i digits.png \
+	--label "digit1" --background "gradient-bg.png" --offset 273x297 --array 10x1 -i digits.png \
+	--label "colon"  --background "gradient-bg.png" --offset 354x297 -i colon.png \
+	--label "digit2" --background "gradient-bg.png" --offset 435x297 --array 10x1 -i digits.png \
+	--label "digit3" --background "gradient-bg.png" --offset 516x297 --array 10x1 -i digits.png \
 	--gci images.gci --header ../software/microbots-arduino/offsets.h
