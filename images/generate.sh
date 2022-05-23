e921f31ef278c5cee7f207a8b1e816a3f97d3851
sanine <sanine.not@pm.me>
2022-05-23 14:27:00 -0500

    add colon

--- images/generate.sh
+++ images/generate.sh
@@ -4,6 +4,7 @@ gci \
 	--label "background" -i gradient-bg.png \
 	--label "digit0" --background "gradient-bg.png" --offset 212x297 --array 10x1 -i digits.png \
 	--label "digit1" --background "gradient-bg.png" --offset 293x297 --array 10x1 -i digits.png \
-	--label "digit2" --background "gradient-bg.png" --offset 374x297 --array 10x1 -i digits.png \
-	--label "digit3" --background "gradient-bg.png" --offset 455x297 --array 10x1 -i digits.png \
+	--label "colon"  --background "gradient-bg.png" --offset 374x297 -i colon.png \
+	--label "digit2" --background "gradient-bg.png" --offset 455x297 --array 10x1 -i digits.png \
+	--label "digit3" --background "gradient-bg.png" --offset 536x297 --array 10x1 -i digits.png \
 	--gci images.gci --header ../software/microbots-arduino/offsets.h
